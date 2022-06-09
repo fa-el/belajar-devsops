577925889eef3adb3131111c9521093c8459f498
fa-el <farly8696@gmail.com>
2022-06-09 13:58:29 +0800

    move html 1 to dir html

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get install -y nginx
-chmod +x /html/index.html
-/html/index.html  > /var/www/html/index.html
+chmod +x ./html/index.html
+./html/index.html  > /var/www/html/index.html
